8160d212a607e68b7dafb5fa73c6de2ffd6cb472
ryan <ryan.williams93@aol.com>
2022-04-05 14:31:33 -0500

    implemented toggle features for cart

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -2,12 +2,37 @@ import React from 'react';
 import CartItem from '../CartItem';
 // We imported the Auth module to conditionally render the checkout button in the JSX.
 import Auth from '../../utils/auth';
+
+import { useStoreContext } from '../../utils/GlobalState';
+import { TOGGLE_CART } from '../../utils/actions';
+
 import './style.css';
 
 const Cart = () => {
+  // You'll use the custom useStoreContext Hook to establish a state variable and the dispatch() function to update the state. In this case, dispatch() will call the TOGGLE_CART action
+  const [state, dispatch] = useStoreContext();
+
+  function toggleCart() {
+    dispatch({ type: TOGGLE_CART });
+  };
+
+  // If cartOpen is false, the component will return a much smaller <div>. 
+  if (!state.cartOpen) {
+    return (
+      // Clicking this <div>, however, will set cartOpen to true and return the expanded shopping cart.
+      <div className="cart-closed" onClick={toggleCart}>
+        {/* You should always wrap emojis (like the shopping cart icon) in a <span> element that includes role and aria-label attributes. Doing so will help screen readers understand the context of the emoji. */}
+        <span
+          role="img"
+          aria-label="trash">🛒</span>
+      </div>
+    );
+  }
+
   return (
     <div className="cart">
-      <div className="close">[close]</div>
+      {/* This handler will toggle the cartOpen value whenever the [close] text is clicked */}
+      <div className="close" onClick={toggleCart}>[close]</div>
       <h2>Shopping Cart</h2>
       <div>
           <CartItem item={{name:'Camera', image:'camera.jpg', price:5, purchaseQuantity:3}} />
